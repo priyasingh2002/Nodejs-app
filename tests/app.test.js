928342f9ab480d136f3fe6b56ed735b7b7514338
priya <priya.singh@thebluespire.com>
2024-11-11 12:58:18 +0530

    second commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,4 +6,17 @@ describe("sample tests", ()=>{
     test("Second test case", ()=>{
         expect(1+2).toBe(3)
     })
+
+
+    test("third test case", ()=>{
+        expect(2+2).toBe(4)
+    })
+
+    test("fourth test case", ()=>{
+        expect(9+2).toBe(11)
+    })
+
+    test("fifth test case", ()=>{
+        expect(10+20).toBe(30)
+    })
 })
