196c883f0314261e26c542dc35f152303276fc4a
priya <priya.singh@thebluespire.com>
2024-11-12 09:49:29 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express=require('express')
+
+const app = express();
+const PORT = 3002;
+
+
+app.use(express.json());
+
+//in memory db for simplicity
+const items={};
+
+//Create item
+app.post('/items',(req,res)=>{
+    const {id, name}=req.body;
+    if(items[id]) return res.status(409).send({message: 'Item already exists'});
+    items[id]={id, name}
+    res.status(201).send(items[id]);
+});
+
+
+//read item
+app.get('/items/:id', (req,res)=>{
+    const item = items[req.params.id];
+    if(!item) return res.status(404).send({message:"item not found"})
+        res.send(item)
+
+});
+
+//update item
+app.put('/items/:id', (req,res)=>{
+    const {id} = req.params;
+    const {name}=req.body;
+    if(!items[id]) return res.status(404).send({message: 'item not found'})
+        items[id].name=name;
+        res.send(items[id]);
+})
+
+//delete item
+app.delete('/items/:id', (req,res)=>{
+    const {id} = req.params;
+    if(!items[id]) return res.status(404).send({message:'item not found'});
+    delete items[id];
+    res.status(204).send();
+})
+
+app.listen(PORT, ()=> console.log(`server running on http://localhost:${PORT}`))
+
+module.exports=app
+
+
+//to delete already pushed nodemodules from github.
+//git rm -r --cached node_modules
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,22 +1,79 @@
-describe("sample tests", ()=>{
-    test("First test case", ()=>{
-        expect(1+1).toBe(2);
+// describe("sample tests", ()=>{
+//     test("First test case", ()=>{
+//         expect(1+1).toBe(2);
+//     })
+
+//     test("Second test case", ()=>{
+//         expect(1+2).toBe(3)
+//     })
+
+
+//     test("third test case", ()=>{
+//         expect(2+2).toBe(4)
+//     })
+
+//     test("fourth test case", ()=>{
+//         expect(9+2).toBe(11)
+//     })
+
+//     test("fifth test case", ()=>{
+//         expect(10+20).toBe(30)
+//     })
+// })
+
+
+
+// import  request from 'supertest'
+// import app from '../index.js'
+
+const request = require('supertest');
+const app = require('../index.js')
+
+describe('CRUD API tests', ()=>{
+    let itemId;
+
+    //test for creating an item
+    it('should create an item', async()=>{
+        const response=await request(app)
+        .post('/items')
+        .send({id: '1', name: 'Test Item' });
+
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toEqual({id:'1', name: 'Test Item'})
+        itemId=response.body.id; //save id for other tests
     })
 
-    test("Second test case", ()=>{
-        expect(1+2).toBe(3)
+
+    //test for reading an item
+    it('should read an item', async()=>{
+        const response=await request(app)
+        .get(`/items/${itemId}`);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({id:'1', name: 'Test Item'})
+   
     })
 
-
-    test("third test case", ()=>{
-        expect(2+2).toBe(4)
+    //test for updating an item
+    it('should update an item', async()=>{
+        const response=await request(app)
+        .put(`/items/${itemId}`)
+        .send({name: 'Updated Item'})
+        expect(response.statusCode).toBe(200);
+        expect(response.body.name).toBe('Updated Item')
+   
     })
 
-    test("fourth test case", ()=>{
-        expect(9+2).toBe(11)
+    //test for deleting an item
+    it('should delete an item', async()=>{
+        const response=await request(app)
+        .delete(`/items/${itemId}`)
+       
+        expect(response.statusCode).toBe(204);
     })
 
-    test("fifth test case", ()=>{
-        expect(10+20).toBe(30)
+    //test for reading a non-existing item
+    it('should return 404 for non-existing item', async()=>{
+        const response=await request(app).get(`/items/${itemId}`);
+        expect(response.statusCode).toBe(404);
     })
 })
