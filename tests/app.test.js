8769a3a4773e0b10ab7b9d7be03ae93d496800d5
priya <priya.singh@thebluespire.com>
2024-11-12 09:53:07 +0530

    fourth commit

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,25 +1,25 @@
-// describe("sample tests", ()=>{
-//     test("First test case", ()=>{
-//         expect(1+1).toBe(2);
-//     })
+describe("sample tests", ()=>{
+    test("First test case", ()=>{
+        expect(1+1).toBe(2);
+    })
 
-//     test("Second test case", ()=>{
-//         expect(1+2).toBe(3)
-//     })
+    test("Second test case", ()=>{
+        expect(1+2).toBe(3)
+    })
 
 
-//     test("third test case", ()=>{
-//         expect(2+2).toBe(4)
-//     })
+    test("third test case", ()=>{
+        expect(2+2).toBe(4)
+    })
 
-//     test("fourth test case", ()=>{
-//         expect(9+2).toBe(11)
-//     })
+    test("fourth test case", ()=>{
+        expect(9+2).toBe(11)
+    })
 
-//     test("fifth test case", ()=>{
-//         expect(10+20).toBe(30)
-//     })
-// })
+    test("fifth test case", ()=>{
+        expect(10+20).toBe(30)
+    })
+})
 
 
 
